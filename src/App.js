f4b41bd7a13b22ef6331bfef1b632368f383ee86
Aleksei Dmitrenko <dieselokj@gmail.com>
2022-05-02 14:42:38 +0300

    Add startTransition usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useTransition } from 'react';
+
+const SlowUI = ({ value }) => (
+  <>
+    {Array(value)
+      .fill(1)
+      .map((_, index) => (
+        <span key={index}>{value - index} </span>
+      ))}
+  </>
+);
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(100000);
+  const [isPending, startTransition] = useTransition();
+
+  const handleClick = () => {
+    setValue(value + 1);
+    startTransition(() => setValue2(value2 + 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>{value}</button>
+      <div
+        style={{
+          opacity: isPending ? 0.5 : 1,
+        }}
+      >
+        <SlowUI value={value2} />
+      </div>
+    </>
   );
 }
 
